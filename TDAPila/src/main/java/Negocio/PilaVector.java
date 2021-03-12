b4cf81f772fe854bb8ab6c2f9631328f14606c79
OlafRealDeal <80548039+OlafRealDeal@users.noreply.github.com>
2021-03-12 16:52:47 -0400

    commit de prueba

--- TDAPila/src/main/java/Negocio/PilaVector.java
+++ TDAPila/src/main/java/Negocio/PilaVector.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Negocio;
 
 /**
@@ -10,5 +5,5 @@ package Negocio;
  * @author Olaf
  */
 public class PilaVector {
-    
+    int x=1;
 }
